c066b915fb7b9b6ed77b4326921bbbd7fc5b71e3
Wingtung Lee <swringtone0704@gmail.com>
2024-02-27 23:15:18 -0800

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,63 +1,100 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+function data_plot(sample) {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    console.log(data);
-  
-  var sample_values = data.sample_values;
-  
-  var otu_label = data.otu_labels;
-  
-  //Sort sample order in descender order 
-  sample_values.sort((a, b) => b - a);
-  console.log(sample_values);
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
-//bar plot
-var otu_ids = data.otu_ids;
-var trace1 = {
-    x: otu_ids.slice(0, 10),
-    y: sample_values.slice(0, 10),
-    type: 'bar',
-    text: otu_ids.slice(0, 10),
-    marker: {
-    color: 'rgb(142,124,195)'
+    // Bubble Chart
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    // Bar Chart
+    var barData =
+    {
+      y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
     }
-  };
+      ;
 
-  var layout = {
-    title: 'Top 10 OTUs',
-    xaxis: { title: 'Otu ID' },
-    yaxis: { title: 'Sample Values' }
-  };
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", [barData], barLayout);
+  });
+}
+
+function init() {
+  // Get the select ID 
+  var selector = d3.select("#selDataset");
+
+  // list of sample is used to names to populate the select options
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    for (var i = 0; i < sampleNames.length; i++) {
+      selector
+        .append("option")
+        .text(sampleNames[i])
+        .property("value", sampleNames[i]);
+    };
+
+    // Use sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    data_plot(firstSample);
+    buildMetadata(firstSample);
 
 
-var data = [trace1];
 
-Plotly.newPlot('bar', data, layout);
-
-
-});
-
-//bubble graph
-var trace1 = {
-  x: otu_ids.slice(0, 10),
-  y: sample_values.slice(0, 10),
-  text: [otu_labels],
-  mode: 'markers',
-  marker: {
-    color: otu_ids,
-    opacity: [1, 0.8, 0.6, 0.4],
-    size: [sample_values]
-  }
+  });
 };
 
-var data = [trace1];
+function buildMetadata(sample) {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
 
-var layout = {
-  title: 'Marker Size',
-  showlegend: false,
-  height: 600,
-  width: 600
-};
+    //demographic info
+    // function buildMetadata(firstSample){
+    var meta = d3.select("#sample-metadata");
+    meta.html('')
+    var samplemeta = data.metadata.find(sampleObj => sampleObj.id == sample);
+    Object.entries(samplemeta).forEach(([key, value]) => {
+      meta.append("h6").text(`${key}:${value}`);
+    });
+  });
+}
 
-Plotly.newPlot('bubbloe', data, layout);
+function optionChanged(newSample) {
+  // get new data each time a new sample is selected
+  data_plot(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init()
